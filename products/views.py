8fd9bc49c04768a031217b25d62cbe0bae749253
MattB859 <mateus756@hotmail.com>
2021-12-01 14:51:53 +0000

    Added sorting

--- products/views.py
+++ products/views.py
@@ -5,16 +5,28 @@ from .models import Product, Category
 
 
 def all_products(request):
-
-    """
-    A view to show all products, including sorting and search queries 
-    """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -29,17 +41,23 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details """
+
+    """
+    A view to show individual product details
+    """
 
     product = get_object_or_404(Product, pk=product_id)
 
