d2c42427e981cd0cc30d3e54d7ae8aeff6cf534c
MattB859 <mateus756@hotmail.com>
2021-12-01 17:36:47 +0000

    Added scroll-up btn back to top link

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,46 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    
+    // slide up Script //
+     $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0});      
+    });
+
+   
+    // owl carousel Script //
+     $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            }, 
+            600:{
+                items: 2,
+                nav: false
+            }, 
+            1000:{
+                items: 3,
+                nav: false
+            } 
+        }
+
+    });          
+});
+
+
