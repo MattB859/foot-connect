a74db7635b64475edd2295601e44a0975d48f334
MattB859 <mateus756@hotmail.com>
2021-12-02 16:57:56 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- foot_connect/urls.py
+++ foot_connect/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 
 urlpatterns = [
+    path('favicon/', favicon_view),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
