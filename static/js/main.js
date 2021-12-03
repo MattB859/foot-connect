f7651fa7a10e678bee6055cae1d5a4584162c826
MattB859 <mateus756@hotmail.com>
2021-12-03 12:29:30 +0000

    Added toasts

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    
-    // slide up Script //
-     $('.scroll-up-btn').click(function(){
-       $('html').animate({scrollTop: 0});      
-    });
-         
 });
 
 
+// slide up Script //
+$('.scroll-up-btn').click(function () {
+    $('html').animate({
+        scrollTop: 0
+    });
+
+});
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
