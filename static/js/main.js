00b26f23c0d0132da26f49c525155c18a35f2e2f
MattB859 <mateus756@hotmail.com>
2021-12-09 18:31:55 +0000

    Added custom styling to allauth login and signup pages

--- static/js/main.js
+++ static/js/main.js
@@ -22,8 +22,3 @@ $('.scroll-up-btn').click(function () {
     });
 
 });
-
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl, option)
-})
