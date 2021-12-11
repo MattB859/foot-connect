0ecccb74b962e70000c97f87f2de73b8b3f20686
MattB859 <mateus756@hotmail.com>
2021-12-11 15:59:56 +0000

    Add delete product function

--- products/views.py
+++ products/views.py
@@ -8,8 +8,10 @@ from .forms import ProductForm
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
 
+    """
+    A view to show all products, including sorting and search queries 
+    """
     products = Product.objects.all()
     query = None
     categories = None
@@ -71,7 +73,10 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a product to the store """
+
+    """
+    Add a product to the store
+    """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -92,12 +97,15 @@ def add_product(request):
 
 
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+
+    """
+    Edit a product in the store
+    """
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
@@ -113,3 +121,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+
+    """
+    Delete a product from the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
