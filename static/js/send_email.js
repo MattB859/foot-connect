d29d67e84787e92010ba5b5dd9c435a90585e1ad
MattB859 <mateus756@hotmail.com>
2021-12-23 16:29:09 +0000

    Added further code refactoring to Curtain Menu

--- static/js/send_email.js
+++ static/js/send_email.js
@@ -1,4 +1,4 @@
-/*function sendMail(contactForm) {
+function sendMail(contactForm) {
   emailjs.send("service_eq7h9b1", "foot_connect", {
       "from_name": contactForm.name.value,
       "from_email": contactForm.emailaddress.value,
@@ -14,27 +14,4 @@
     );
     contactForm.reset();
   return false;
-}*/
-
-const btn = document.getElementById('button');
-
-document.getElementById('form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    btn.value = 'Sending...';
-
-    const serviceID = 'default_service';
-    const templateID = 'foot_connect';
-
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-      }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-      });
-      ;
-    return contactForm.reset();
-  });
+}
