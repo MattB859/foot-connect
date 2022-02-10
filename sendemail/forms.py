701a170241f267075559efaf27c26bf33b1174ae
MattB859 <mateus756@hotmail.com>
2022-02-10 15:48:22 +0000

    Deployment

--- sendemail/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    """ email form """
-
-    name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
--- sendemail/views.py
+++ sendemail/views.py
@@ -1,31 +1,13 @@
 # sendemail/views.py
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from .forms import ContactForm
+from django.conf import settings
 
-
-#def contact_view(request):
-    #if request.method == 'GET':
-        #form = ContactForm()
-    #else:
-        #form = ContactForm(request.POST)
-        #if form.is_valid():
-            #name = form.cleaned_data['name']
-            #emailaddress = form.cleaned_data['emailaddress']
-            #message = form.cleaned_data['message']
-            #try:
-                #send_mail(
-                    #name, message,
-                    #emailaddress,
-                    #['gmail'])
-            #except BadHeaderError:
-                #return HttpResponse('Invalid header found.')
-            #return redirect('success')
-    #return render(request, "contacts/email.html", {'form': form})
 
 def contact_view(request):
     if request.method == 'POST':
+        print(request.POST)
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
@@ -40,7 +22,7 @@ def contact_view(request):
 
         From: {}
         '''.format(data['message'], data['email'])
-        send_mail(data['name'], message, '', ['EMAIL_HOST_USER'])
+        send_mail(data['name'], message, settings.DEFAULT_FROM_EMAIL, ['mattburrellmusic@gmail.com'])
 
         return redirect('success')
     return render(request, "contacts/email.html", {})
